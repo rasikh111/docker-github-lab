a8aa88c0145deef9cfa2c44b075fa7153af39609
Muhammad Rasikh Riaz <mo.rasikhriaz@gmail.com>
2025-09-03 22:11:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Docker! RASIKH Version 2 "
+    return "Hello, Docker! RASIKH RIAZ tag auto incremented according to the workflow number"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
